6c9cd0d57f27d4f8f08c6e931b2dcdde18f879dc
amlopez29 <amlopez290@gmail.com>
2021-10-04 05:19:01 -0500

    fixing indentations

--- nest_devices.py
+++ nest_devices.py
@@ -9,30 +9,30 @@ class thermostat:
 
 
 
-	def __init__(self, device_id, access_token):
-		self.device_id = device_id
-		self.access_token = access_token
-		self.info = None
-		self.api_headers = {
-			'Content-Type': 'application/json',
-			'Authorization': 'Bearer {}'.format(self.access_token)
-		}
+    def __init__(self, device_id, access_token):
+        self.device_id = device_id
+        self.access_token = access_token
+        self.info = None
+        self.api_headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
 
-	def get_info(self):
-		get_info_thermostat_api_url = "enterprises/{}/devices/".format(project_id)
+    def get_info(self):
+        get_info_thermostat_api_url = "enterprises/{}/devices/".format(project_id)
 
-		self.info = requests.get(api_endpoint+get_info_thermostat_api_url+self.device_id, headers=self.api_headers)
-		return self.info
+        self.info = requests.get(api_endpoint+get_info_thermostat_api_url+self.device_id, headers=self.api_headers)
+        return self.info
 
-	def set_cool(self, temp_celcius):
-		set_cool_api_url = "enterprises/{}/devices/".format(project_id)
-		command = {
-  			"command" : "sdm.devices.commands.ThermostatTemperatureSetpoint.SetCool",
-  			"params" : {
-    			"coolCelsius" : temp_celcius
-  			}
-		}
+    def set_cool(self, temp_celcius):
+        set_cool_api_url = "enterprises/{}/devices/".format(project_id)
+        command = {
+            "command" : "sdm.devices.commands.ThermostatTemperatureSetpoint.SetCool",
+            "params" : {
+                "coolCelsius" : temp_celcius
+            }
+        }
 
-		requests.post(api_endpoint+set_cool_api_url+self.device_id+":executeCommand", json=command, headers=self.api_headers)
+        requests.post(api_endpoint+set_cool_api_url+self.device_id+":executeCommand", json=command, headers=self.api_headers)
 
 
