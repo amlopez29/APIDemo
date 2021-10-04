92282a53678397945babe3f9f50334b9b7a72ab3
amlopez29 <amlopez290@gmail.com>
2021-10-04 05:23:51 -0500

    Typos

--- nest_devices.py
+++ nest_devices.py
@@ -1,13 +1,11 @@
 import requests
 
-#any nest device can be added to this module at a later date.
+#Any nest device can be added to this module at a later date.
 
 api_endpoint = "https://smartdevicemanagement.googleapis.com/v1/"
 project_id = "39831c32-d4d0-4f03-b6ce-80a04045a2ad"
 
 class thermostat:
-
-
 
     def __init__(self, device_id, access_token):
         self.device_id = device_id
@@ -24,15 +22,13 @@ class thermostat:
         self.info = requests.get(api_endpoint+get_info_thermostat_api_url+self.device_id, headers=self.api_headers)
         return self.info
 
-    def set_cool(self, temp_celcius):
+    def set_cool(self, temp_celsius):
         set_cool_api_url = "enterprises/{}/devices/".format(project_id)
         command = {
             "command" : "sdm.devices.commands.ThermostatTemperatureSetpoint.SetCool",
             "params" : {
-                "coolCelsius" : temp_celcius
+                "coolCelsius" : temp_celsius
             }
         }
 
         requests.post(api_endpoint+set_cool_api_url+self.device_id+":executeCommand", json=command, headers=self.api_headers)
-
-
