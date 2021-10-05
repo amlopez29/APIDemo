0f859b2f76b9bc7b1c545b36c9b6a4afe698407d
amlopez29 <amlopez290@gmail.com>
2021-10-04 23:32:43 -0500

    Update module to use AWS SSM Parameter Store
    Secrets no longer stored in plaintext.

--- nest_tokens.py
+++ nest_tokens.py
@@ -1,17 +1,39 @@
 """
-This is the quick solution to handling OAuth tokens. Ideally something like this could be handled by AWS KMS. 
-This solution stores a secret token in plaintext which is obviously not ideal or best practice as it exposes a key security component.
+This module has been updated to use AWS SSM Parameter Store. 
+This not only allows for configuration updates but also stores secrets in an encrypted state.
 """
 import requests
+import json
+import boto3
+ssm = boto3.client('ssm', region_name="us-east-1")
 
 #Tokens and IDs
-google_oauth_id = "854305848149-qrd4mep639e8pk6448t101tavnenbgve.apps.googleusercontent.com"
-google_refresh_token = "1//0fc_ydCTb5GSgCgYIARAAGA8SNwF-L9IrxyvMMXW0y7_wRFv75HFwAAcSACk1sqYMlYj53qdUnRLcKJqIfWOqRBD0sYG43yIvsBI"
-google_secret = "TTSygcDJqeARhtM7T9J9YbIv"
-refresh_url = "https://www.googleapis.com/oauth2/v4/token?client_id={}&client_secret={}&refresh_token={}&grant_type=refresh_token".format(google_oauth_id, google_secret, google_refresh_token)
+google_oauth_id = None
+google_refresh_token = None
+google_secret = None
+
+
+def get_nest_token_parameters():
+    global google_secret
+    google_secret_param = ssm.get_parameters(Names=["nest_secret"], WithDecryption=True)
+    google_secret_param = google_secret_param.get("Parameters")
+    google_secret = google_secret_param[0].get("Value")
+    
+    global google_oauth_id
+    oauth_id_param = ssm.get_parameters(Names=["nest_oauth_id"])
+    oauth_id_param = oauth_id_param.get("Parameters")
+    google_oauth_id = oauth_id_param[0].get("Value")
+    
+    global google_refresh_token
+    refresh_token_param = ssm.get_parameters(Names=["nest_refresh_token"], WithDecryption=True)
+    refresh_token_param = refresh_token_param.get("Parameters")
+    google_refresh_token = refresh_token_param[0].get("Value")
+    
 
 def get_access_token():
     #Retrieve token
+    get_nest_token_parameters()
+    refresh_url = "https://www.googleapis.com/oauth2/v4/token?client_id={}&client_secret={}&refresh_token={}&grant_type=refresh_token".format(google_oauth_id, google_secret, google_refresh_token)
     access_token_json = requests.post(refresh_url)
     access_token_json = access_token_json.json()
     google_access_token = access_token_json.get("access_token")
